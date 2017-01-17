c2ba63be8cce644dae9bf46eb4c71c61307e4697
NathanIng <ing.nathany@gmail.com>
2017-01-16 22:39:04 -0800

    Added reassembly code to data.py Finished transplanting the assemble functions I'm still not really satisfied with the implementation of assemble()
    Wrote down pseudo code for an alternative tiling procedure.
    It' just refactoring the current procedure based on some performacen assumptions.

--- data.py
+++ data.py
@@ -21,6 +21,17 @@ import colorNormalization as cnorm
 import numpy as np
 import os
 import shutil
+import inspect
+
+def PrintFrame():
+    callerframerecord = inspect.stack()[1] #0 represents this line
+    frame = callerframerecord[0]
+    info = inspect.getframeinfo(frame)
+    thisfile = info.filename
+    thisfun = info.function
+    thisline = info.lineno
+
+    return '{} in {} @ {}'.format(thisfile, thisfun, thisline)
 
 def pull_svs_stats(svs):
     # Specficially for svs files
@@ -61,6 +72,36 @@ def update_map(tilemap, x, y, value):
     tilemap[y,x] = value
 
     return tilemap
+
+
+
+'''
+REVISED PSEUDOCODE 
+TODO: IMPLEMENT THIS
+
+it's the opposite of the assembly code.
+
+1. decide how many rows and columns
+    - intialize a counting variable
+    - initialize a map variable
+2. for each row:
+    1. load the row
+    2. move along the row one tilesize - determine if it passes a check
+    3. if it passes the check, write it, increment the counting variable
+    4. Record the position of the passing tile in the map variable
+    5. continue until the whole number of tiles is exhausted
+
+Then, do some A/B tests to see which one's faster !!!!
+The point is to minimize the number of calls to OpenSlide.read_region()
+The theory is that read_region() is relatively slow when invoked in serial
+and that it's performance is overall faster in reading large chunks at once.
+Another piece of assumption is indexing into the np array is faster than read_region()
+
+Maybe there's a performance curve depending on the tile size, or just the number of reads
+which is a function of tile size, and the total image size. IDK.
+
+Maybe it doesn't matter. It's just bothering me. 
+'''
 
 def tile_wsi(wsi, tilesize, writesize, writeto, prefix = 'tile'):
     '''
@@ -146,11 +187,6 @@ def find_bcg(wsi):
 def training():
     pass # make_data.py
 
-def inference():
-
-    pass
-
-
 def create_dirs_inference(filename, writeto, sub_dirs = ['tiles', 'result', 'labels', 'prob'], remove = False):
     tail = os.path.basename(filename)
     slide_name, ex = os.path.splitext(tail)
@@ -186,6 +222,162 @@ def run_inference(filename, writeto, tilesize = 512, writesize = 256, remove_fir
     wsi.close()
 
     return tiledir, result_root
+
+
+
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~ ASSEMBLE TILES ~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+
+def label_regions(m):
+    h,w = m.shape
+    bw = m > 0
+    bw.dtype = np.uint8 # can't keep bool
+
+    contours, _ = cv2.findContours(image = bw, mode = cv2.RETR_EXTERNAL, 
+            method = cv2.CHAIN_APPROX_SIMPLE)
+    ll = np.zeros(shape = (h,w), dtype = np.uint8)
+    for idx, ct in enumerate(contours):
+        cv2.drawContours(ll, [ct], 0, idx+1, thickness = -1) # thickness -1 means to fill
+
+    return ll, contours
+    
+
+def get_all_regions(m, threshold = 40):
+    '''
+    Spit out regions as bounding rects of connected components
+    threshold is the number of tiles required (should be careful of large tilesizes)
+    '''
+    regions = []
+
+    ll, contours = label_regions(m)
+
+    for idx, ct in enumerate(contours):
+        if (ll == idx+1).sum() >= threshold:
+            regions.append(cv2.boundingRect(ct))
+
+    return regions
+
+
+def partition_rows(m):
+    rows = []
+    n_rows = m.shape[0]
+    for ix in range(n_rows):
+        rows.append(m[ix, :])
+
+    return rows
+
+def empty_block(place_size):
+    return np.zeros(shape = (place_size, place_size, 3), dtype = np.uint8)
+
+
+def build_row(row, source_dir, place_size,  overlay_dir = ''):
+    do_overlay = os.path.exists(overlay_dir)
+    #print PrintFrame(),
+    #print "do_overlay = {}".format(do_overlay)
+
+    #TODO See if we can do it without initializing with empty col on the left :
+    row_out = empty_block(place_size)
+    for r in row:
+        if r == 0:
+            block = empty_block(place_size)
+        else:
+            # TODO fix the hard-coded "tile" prefix and extension suffix: 
+            pth = os.path.join(source_dir, 'tile{}.jpg'.format(r))
+            block = cv2.imread(pth)
+
+            if block is None:
+                print PrintFrame(),
+                print "img {} ought to exist but I guess doesn't.".format(pth)
+                block = empty_block(place_size)
+
+            block = cv2.resize(block, dsize = (place_size, place_size))
+
+            if do_overlay:
+            # TODO fix the hard-coded "tile" prefix and extension suffix: 
+                ov_pth = os.path.join(overlay_dir, 'tile{}.jpg'.format(r))
+                img = cv2.imread(ov_pth)
+                img = cv2.resize(img, dsize = (place_size, place_size))
+                block = color_image(img, block)
+
+        row_out = np.append(row_out, block, axis=1) # Sketchhyyyy
+
+    return row_out
+    
+def assemble_rows(rows):
+    img = rows[0]
+    for r in rows[1:]:
+        img = np.append(img, r, axis=0)
+    return img
+
+
+def downsize_keep_ratio(img, target_w = 1024, interp = cv2.INTER_LINEAR):
+    factor = img.shape[1]/float(target_w)
+    target_h = int(img.shape[0] / factor)
+
+    img_new = cv2.resize(src = img, dsize = (target_w, target_h), interpolation = interp)
+
+    return img_new
+
+
+def build_region(region, m, source_dir, place_size, overlay_dir, max_w = 10000):
+    x,y,w,h = region
+    
+    print PrintFrame(),
+    print " x: {} y: {} w: {} h: {}".format(x,y,w,h)
+    # Check if the output will be large
+    if w*h*(place_size**2) > (2**31)/3:
+        # edit place_size so that the output will fit:
+        print PrintFrame(),
+        print "Found region > 2**31, resizing to ",
+        place_size = int(np.sqrt(((2**31)/3) / (w*h)))
+        print "{}".format(place_size)
+
+    rows = partition_rows(m[y:y+h, x:x+w])
+    
+    built_img = [] # Not really an image; a list of row images
+    for ix, r in enumerate(rows):
+        #print PrintFrame(),"Row {} / {}".format(ix, len(rows))
+        row = build_row(r, source_dir, place_size, overlay_dir)
+        built_img.append(row)
+
+    img = assemble_rows(built_img)
+
+    # Resize down to something sane for writing:
+    if img.shape[1] > max_w:
+        img = resize_keep_ratio(img, max_w)
+    return img
+
+
+# TODO this function isn't very good. the place to generalize isn't really obvious to me. 
+def assemble(result_root, tile_sources = ['result', 'labels', 'prob'],
+        interps = [cv2.INTER_LINEAR, cv2.INTER_NEAREST, cv2.INTER_LINEAR],
+        img_overlay = ['x', 'x', 'x']):
+    # Pull in the image map:
+    map_file = os.path.join(result_root, 'data_tilemap.npy')
+    m = np.load(map_file)
+    [N,M] = m.shape # Forego printing
+
+    # Pull out disconnected regions that pass a size cutoff:
+    regions = get_all_regions(m, threshold = 5)
+
+    for index, reg in enumerate(regions):
+        # TODO this loop still sucks
+        #print PrintFrame(),"Processing region {}".format(index)
+        for tile_src, interp, overlay in zip(tile_sources, interps, img_overlay):
+            img_name = '{}_{:03d}.jpg'.format(tile_src, index) 
+            source_dir = os.path.join(result_root, tile_src)
+            overlay_dir = os.path.join(result_root, overlay)
+            print "Pulling images from {}".format(source_dir)
+            print "Overlaying onto {}".format(overlay_dir)
+            img = build_region(reg, m, source_dir, 256, overlay_dir) 
+            
+            img_name = os.path.join(result_root,img_name)
+            cv2.imwrite(img_name, img)
+
+
+
+
 
 
 
--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,5 @@
 '''
+
 Collection of run() functions from data, training and histoseg
 
 '''
@@ -13,15 +14,19 @@ def make_data_inference(filename, writeto, tilesize, writesize, remove_first = F
     return data.run_inference(filename, writeto, tilesize, writesize, remove_first)
 
  
+def assemble_tiles(result_root, source_dirs):
+    print "Assembling tiles:"
 
-
+    data.assemble(result_root, source_dirs)
 
 if __name__ == '__main__':
-   filename = '/home/nathan/data/pca_wsi/MaZ-001-a.svs'
-   writeto = '/home/nathan/histo-seg/pca'
-   tilesize = 2048
-   writesize = 256
-   remove_first = True
+    filename = '/Users/nathaning/Dropbox/SVS/PCA/MaZ.svs'
+    writeto = '/Users/nathaning/histo-seg/pca'
+    tilesize = 2048
+    writesize = 256
+    remove_first = True
 
-   tiledir, exproot = make_data_inference(filename, writeto, tilesize, writesize, remove_first)
-    
+    #tiledir, exproot = make_data_inference(filename, writeto, tilesize, writesize, remove_first)
+  
+    exproot = '/Users/nathaning/histo-seg/pca/MaZ'
+    assemble_tiles(exproot, ['result'])
