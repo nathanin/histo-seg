51b577e1e855406d0509642612df83d8003d5d32
nathan <ing.nathany@gmail.com>
2017-03-30 18:19:36 -0700

    saving work

--- save.sh
+++ save.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-# Parse arguments
+# Do a setting to override the base git message if wanted
 
 set -e
 
-doPush=false
+git commit -a -m 'saving work'
+git push origin master
 
-while [[ $# -gt 1 ]]
-do
-    key="$1"
-
-    case $key in
-        -p|--push)  # Push or not. Default not push
-        doPush=true
-        echo "Setting push true"
-        shift
-        ;; # Done with one case
-    *)
-        # Unknown option
-        ;;
-esac
-shift # past argument
-done
-
-git commit -a
-
-if [ "$doPush"=true ]; then
-    git push origin master
-fi
+exit 0
