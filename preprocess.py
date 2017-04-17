3cf28f0b33df7919a59118e2c12eaa8e6f40c3d9
NathanIng <ing.nathany@gmail.com>
2017-04-17 16:12:45 -0700

    Made functional template for validate
    - validate checks global accuracy and per-class accuracies
    - preprocess will create a tilemap for better (eventual) classification

---
+++ preprocess.py
@@ -0,0 +1,28 @@
+'''
+Preprocessing for the whole slide image
+
+includes separating tissue from background
+and processable from non-processable tissue
+
+use a white threshold to determine tissue / background
+use an AlexNet to determine processable / non-processable tissue
+
+changelog
+---------
+2017-04-18:
+    Initial
+
+'''
+
+'''
+Functions
+
+
+
+
+'''
+
+
+
+
+
