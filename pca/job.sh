ab6e45ab070547773acd7114d8e59c6747c0d364
Nathan <ing.nathany@gmail.com>
2017-05-03 17:11:56 -0700

    update qsub script

--- pca/job.sh
+++ pca/job.sh
@@ -3,6 +3,8 @@
 set -e
 module load openslide/3.4.1
 
+histoseg=/home/ingn/histo-seg
+
 echo $1
 
-
+python $histoseg/core/histo_pipeline.py
--- pca/submitjobs.sh
+++ pca/submitjobs.sh
@@ -5,6 +5,6 @@ set -e
 svsdir="/home/ingn/wsi/pca"
 
 for svs in $(ls $svsdir/*.svs); do
-    qsub -V ./job.sh $svs  
+    qsub -V -cwd -N slide-$(basename $svs) ./job.sh $svs  
 done
 
